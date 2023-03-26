4e76ec6147405c83f3203301efe1d32460ab17c2
Djulian Campos <88363369+juliancamprr@users.noreply.github.com>
2023-03-25 22:41:30 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,20 +3,23 @@ const pokeButton = document.querySelector("#pokebutton")
 const namePoke = document.querySelector(".name")
 const habilities = document.querySelector(".habilidades")
 const altura = document.querySelector(".altura")
+const img = document.querySelector("img")
 const error = document.querySelector(".error")
 
 pokeButton.addEventListener('click', () => {
-    const pokeName = pokeInput.value;
+    const pokeName = pokeInput.value.toLowerCase();
     const url = `https://pokeapi.co/api/v2/pokemon/${pokeName}`
 
     fetch(url).then(response => {
         return response.json()
     }).then(data => {
         console.log(data)
-
+        const pokeImg = data.sprites.front_default
         namePoke.innerHTML = `Nome: ${data.name}`
-        habilities.innerHTML = `Habilidades: ${data.moves.map(move => move.move.name).slice(0,3).join(', ')}`
+        habilities.innerHTML = `Habilidades: ${data.moves.map(move => move.move.name).slice(0,5).join(', ')}`
         altura.innerHTML = `Altura: ${data.height}`
+        img.src = pokeImg
+        error.innerHTML = ''
     }).catch(err => {
         error.innerHTML = 'Erro'
     })
